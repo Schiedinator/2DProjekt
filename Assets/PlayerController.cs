7f5aabfa32b48e37a80a271b9f4d6dbc62527720
M S <maximilian.schieder@gmx.net>
2020-04-27 17:50:18 +0200

    v1
    Test

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, 0);
+        }
+        
+    }
+}
